a18b1d4c5880e068dd9f583269779e4754eaad0d
Walt Mankowski (macbook pro) <waltman@pobox.com>
2019-05-29 14:31:13 -0400

    Initial Commit

---
+++ MazeDraw/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  MazeDraw
+//
+//  Created by Walter Mankowski on 5/29/19.
+//  Copyright © 2019 Walter Mankowski. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
